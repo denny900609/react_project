6d3999c4960fb35f24360519591e944182294bf2
denny900609 <denny9269913@gmail.com>
2021-04-01 11:56:37 +0800

    0401H.W

---
+++ App1.js
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
+function Header() {
+  return (
+    <div>
+      <h1>New Header</h1>
+    </div>
+  );
+}
+function Content() {
+  return (
+    <div>
+      <h2>New Content</h2>
+      <p>Hello World!!!</p>
+    </div>
+  );
+}
+export default App;
---
+++ App2.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+import React from 'react';
+
+const App = () => (
+    <div>
+      <Header />
+      <Content />
+    </div>
+);
+
+const Header = () => (
+    <div>
+      <h1>New Header</h1>
+    </div>
+);
+
+const Content = () => (
+    <div>
+      <h2>New Content</h2>
+      <p>Hello World!!!</p>
+    </div>
+);
+
+export default App;
